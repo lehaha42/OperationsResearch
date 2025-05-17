455852e0564d48fc6eb089317257ff30e2d617da
lehaha42 <146331806+lehaha42@users.noreply.github.com>
2025-05-17 14:37:17 +1000

    comments

--- t6/main.py
+++ t6/main.py
@@ -1,17 +1,21 @@
 from pulp import *
 
 
+# отображение судоку
 def show(arr, pos_arr):
+    # общий размер
     size = [0, 0]
     for p in pos_arr:
         size = [max(size[0], p[0]+9), max(size[1], p[1]+9)]
 
+    # какие клетки показывать
     s = [[-1 for _ in range(size[1])] for _ in range(size[0])]
     for n in range(len(arr)):
         for i in range(9):
             for j in range(9):
                 s[i+pos_arr[n][0]][j+pos_arr[n][1]] = arr[n][i][j]
 
+    # вывод
     for i in range(size[0]):
         if i % 3 == 0 and i != 0:
             print()
@@ -25,28 +29,36 @@ def show(arr, pos_arr):
         print()
 
 
+# решение
 def solve(data):
+    # главная проблема
     prob = LpProblem("sudoku")
 
+    # переменные
     pos_arr = []
     arr = []
     connections = []
     choices = [LpVariable.dicts(f"Choice_{i}", (range(9), range(9), range(1, 10)), cat=LpBinary) for i in range(len(data))]
 
+    # для каждой сетки судоку
     for num in range(len(data)):
 
+        # одна цифра на клетку
         for i in range(9):
             for j in range(9):
                 prob += lpSum([choices[num][i][j][k] for k in range(1, 10)]) == 1
 
+        # уникальные цифры в строке
         for i in range(9):
             for k in range(1, 10):
                 prob += lpSum([choices[num][i][j][k] for j in range(9)]) == 1
 
+        # уникальные цифры в столбце
         for j in range(9):
             for k in range(1, 10):
                 prob += lpSum([choices[num][i][j][k] for i in range(9)]) == 1
 
+        # уникальные цифры в квадрате 3х3
         for r in range(3):
             for c in range(3):
                 for k in range(1, 10):
@@ -54,12 +66,14 @@ def solve(data):
                                    for i in range(r*3, (r+1)*3)
                                    for j in range(c*3, (c+1)*3)]) == 1
 
+        # ввод изначальных значений
         for i in range(9):
             for j in range(9):
                 if data[0]['arr'][i][j] != 0:
                     k = data[0]['arr'][i][j]
                     prob += choices[num][i][j][k] == 1
 
+        # оброаботка пересечений
         p = data[num]['pos']
         for n in range(len(pos_arr)):
             pos = pos_arr[n]
@@ -70,16 +84,20 @@ def solve(data):
         pos_arr.append(p)
         arr.append(data[num]['arr'])
 
+    # условие равенства пересекающихся клеток
     for g1, i1, j1, g2, i2, j2 in connections:
         for k in range(1, 10):
             prob += choices[g1][i1][j1][k] == choices[g2][i2][j2][k]
 
+    # решатель
     prob.solve()
 
+    # проверка наличия решения
     if LpStatus[prob.status] != "Optimal":
         print("ERROR")
         return [arr, [], pos_arr]
 
+    # преобразование решения для отображения
     solutions = [[[0 for _ in range(9)] for _ in range(9)] for _ in range(len(data))]
     for n in range(len(data)):
         for i in range(9):
@@ -92,6 +110,7 @@ def solve(data):
     return [arr, solutions, pos_arr]
 
 
+# вывод исходного судоку и решения
 def dispay(solution):
     print("\nquest:\n")
     show(solution[0], solution[2])
@@ -99,6 +118,7 @@ def dispay(solution):
     show(solution[1], solution[2])
 
 
+# главная функция
 def main():
     solutions = []
     with open("data.json", "r") as file:
